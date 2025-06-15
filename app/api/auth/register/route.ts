5da6fa59f9b71ffb4a0ce9a3bb6451f9086c30cc
gyawali9 <roshangyawali9@gmail.com>
2025-06-15 12:41:26 +0545

    feat: nextAuth setup

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,56 @@
+import { connectToDb } from "@/app/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/app/models/User";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          error: "Email and password are required",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    await connectToDb();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "User already registered",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // create user
+    await User.create({
+      email,
+      password,
+    });
+    return NextResponse.json(
+      {
+        message: "User registered successfully",
+        success: true,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error, "registration error");
+    return NextResponse.json(
+      {
+        error: "Failed to register user",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
